0685e74dbd69ce97f9ab3fb72f6b448f51ed400a
igorbrasan <igorbraga.rsantos@gmail.com>
2021-08-23 10:35:01 -0300

    Relatorios

--- py/MetodoBisseccao/relatorios.py
+++ py/MetodoBisseccao/relatorios.py
@@ -1,7 +1,15 @@
 #imprime a iretação caso não encontre a raiz
 def iterar(ctr, f, xi):
     return("Iteração {} . Xi = {} , f(xi) = {}" .format(ctr,xi,f(xi)))
+
+#imprime resulado na tela
 def resultado(xi):
     return("O valor da Raiz é: {}" .format(xi))
+
+#criar um arquivo para colocar as coisas
 def gerarArquivo(filename):
     f = open("{}.txt".format(filename), "a")
+
+#escreve a sessão num arquivo
+def relatar(filename):
+    #estudar em https://www.w3schools.com/python/python_file_write.asp
