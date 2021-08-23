986256bc8911bed033dc4d3d9fd4062c2c20cf21
igorbrasan <igorbraga.rsantos@gmail.com>
2021-08-23 09:29:13 -0300

    modularizando o script de bissecção

---
+++ py/MetodoBisseccao/iterar.py
@@ -0,0 +1,3 @@
+#imprime a iretação caso não encontre a raiz
+def bisseccao(ctr, f, xi):
+    print("Iteração {} . Xi = {} , f(xi) = {}" .format(ctr,xi,f(xi)))
--- py/MetodoBisseccao/main.py
+++ py/MetodoBisseccao/main.py
@@ -1,5 +1,6 @@
 #importar biblioteca matematica
 import math
+from iterar import bisseccao
 
 #definir um intervalo [a,b] e um erro e
 a = float(2)
@@ -9,10 +10,6 @@ ctr = int(0)
 #definir uma função
 def f(x):
     return x ** 2 - 5
-
-#imprime a iretação caso não encontre a raiz
-def iterar(ctr, f, xi):
-    print("Iteração {} . Xi = {} , f(xi) = {}" .format(ctr,xi,f(xi)))
 
 #Testando teorema de Bolzano
 
@@ -25,7 +22,7 @@ if f(a) * f(b) < 0:
             print("A raiz é: {}" .format(xi))
             break
         else:
-            iterar(ctr, f, xi)
+            bisseccao(ctr, f, xi)
             if f(a) * f(xi) < 0:
                 b = xi
             else:
